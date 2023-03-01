a33d6cee5442bf1e5217f445909a1a6ea14a3234
alanalzi <117884402+alanalzi@users.noreply.github.com>
2023-03-01 17:48:06 +0700

    Add files via upload

--- 2_D_71220824.py
+++ 2_D_71220824.py
@@ -1,3 +1,4 @@
 ang1 = int(input("Masukan angka: "))
 pangkat2 = lambda ang1: ang1**2
-print("Hasil kuadrat dari",ang1,"adalah:", pangkat2(ang1))
+tot = pangkat2(ang1)
+print("Hasil kuadrat dari",ang1,"adalah:", tot)
